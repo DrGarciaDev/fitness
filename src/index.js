232215c05b39052a236a8da924fa288a3312f595
Luis García <luis_grdz@hotmail.com>
2019-09-17 01:03:43 -0500

    Hasta el video 7: eliminación de algunos archivos y primeros ejercicios para comprender los témas más a fondo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
+// const element = document.createElement('h1')
+// element.innerText = 'Hello World'
+// // eslint-disable-next-line no-unused-vars
+// const container = document.getElementById('root')
+// container.appendChild(element)
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const nombre = 'Luis';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const user = {
+    firstName: 'Luis',
+    lastName: 'García',
+    avatar: 'Iagen'
+}
+
+function getName(user) {
+    return `${ user.firstName } ${ user.lastName }`
+}
+
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello { getName(user) }</h1>
+    }
+    return <h1>Hello Extraño</h1>
+}
+
+const element = (
+    <div>
+        <h1>{getGreeting(user)}</h1>
+    </div>
+)
+
+// const element = <h1>{ getGreeting(user) }</h1>
+// haciendo uso de la sintaxis de JSX
+
+const container = document.getElementById('root')
+
+// ReactDOM.render(__QUE__, __DONDE__)
+ReactDom.render(element, container);

4102f448ecc7539fb50c1af02c331e0143a01570
Luis García <luis_grdz@hotmail.com>
2019-09-19 22:49:36 -0500

    Video 10: agregando estilos css y bootstrap con npm

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,23 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import imagen from '../images/exercise.png';
+import './styles/Card.css';
 
 class Card extends React.Component {
     render () {
         // return <h1>Card</h1>
         return (
-            <div>
-                <div>
-                    <img src={imagen} />
-                </div>
-                <div>
-                    <h1>Technique Guides</h1>
-                    <p>Learn amazong street</p>
+            <div className="card mx-auto Fitness-Card">
+                <div className="card-body">
+                    <div className="row center">
+                        <div className="col-6">
+                            <img src={imagen} className="float-right" />
+                        </div>
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1>Technique Guides</h1>
+                            <p>Learn amazong street</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Card from './components/Card';
-
+import 'bootstrap/dist/css/bootstrap.css';
 
 
 const container = document.getElementById('root')

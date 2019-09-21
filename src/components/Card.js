9d69228d78f7bb2dc0b76431e0da5d9ebf78f0dc
Luis García <luis_grdz@hotmail.com>
2019-09-21 14:32:49 -0500

    Video 11: Entendiendo y aplicando las props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,31 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
-import imagen from '../images/exercise.png';
+// import imagen from '../images/exercise.png';
+import circleImg from '../images/circles.png';
+
 import './styles/Card.css';
 
 class Card extends React.Component {
     render () {
         // return <h1>Card</h1>
+        // Haciendo destructuring para evitar escribir en todas las props utilizadas el this.props
+        const { title, description, img, leftColor, rigthtColor } = this.props;
+
         return (
-            <div className="card mx-auto Fitness-Card">
+            <div className="card mx-auto Fitness-Card" 
+                /* estilos en linea  */
+                style={{
+                    backgroundImage: `url(${ circleImg }), linear-gradient(to right, ${ leftColor }, ${ rigthtColor })`
+                }}
+            >
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={imagen} className="float-right" />
+                            <img src={ img } className="float-right" />
                         </div>
                         <div className="col-6 Fitness-Card-Info">
-                            <h1>Technique Guides</h1>
-                            <p>Learn amazong street</p>
+                            <h1>{ title }</h1>
+                            <p>{ description }</p>
                         </div>
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -13,4 +13,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 const container = document.getElementById('root')
 
 // ReactDOM.render(__QUE__, __DONDE__)
-ReactDom.render(<Card />, container);
+ReactDom.render(<Card
+                        title="Technique Guides"
+                        description="Learn amazong street"
+                        img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06
+import React from 'react'"
+                        leftColor="#A74CF2"
+                        rigthtColor="#617BFB"
+                        />, container);
